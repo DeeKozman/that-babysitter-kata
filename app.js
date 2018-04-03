36124446b097ba0a27a505062340e5fb76fd4f96
DeeKozman <dankoscielak@gmail.com>
2018-04-03 13:32:13 -0400

    added more basic tests and added basic functionality in app.js

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
-module.exports = {
-    sayHelloWorld: function() {
-    return 'hello world';
+'use strict'
+
+// Const
+const startTimeToBedtimePay = 12;
+const bedtimeToMidnightPay = 8;
+const midnightToEndOfJobPay = 16;
+
+module.exports = function (start, finish, bed){
+    var startTime = transformHours(start.getHours());
+    var finishTime = transformHours(finish.getHours());
+    var bedTime = transformHours(bed.getHours());
+    var pay = 0;
+    for (var i = startTime; i < finishTime; i++) {
+        pay += findTheRate(i, bedTime);
+      }
+      
+      /*console.info('Start-time: ' + transformHours(start.getHours()));
+      console.info('Finish-time: ' + transformHours(finish.getHours()));
+      console.info('Bedtime:    ' + transformHours(bed.getHours()));
+      console.info('-----------------')
+      console.info(`Total Pay:   $${pay}`)*/
+      return pay;
+      
+};
+
+function findTheRate (hour, bedTime) {
+    if (hour < 12 && hour < bedTime) {
+      return startTimeToBedtimePay;
+    } else if (hour < 12 && bedTime <= hour) {
+      return bedtimeToMidnightPay
+    } else {
+      return midnightToEndOfJobPay;
     }
-}
+  }
+  
+  function transformHours (time) {
+    return time < 5 ? time + 12 : time;
+  }
--- test/appTest.js
+++ test/appTest.js
@@ -1,10 +1,82 @@
-const assert = require('chai').assert;
-const sayHelloWorld = require('../app').sayHelloWorld;
-//const app = require('../app');
+'use strict';
 
-describe('App', function(){
-    it('app should return hello world', function(){
-        let result = sayHelloWorld();
-        assert.equal(result, 'hello world');
+const assert = require('chai').assert;
+const app = require('../app');
+const startTime = require("../app").startTime;
+const finishTime = require("../app").finishTime;
+const bedTime =  require("../app").bedTime;
+const pay = require("../app").pay;
+const startTimeToBedtimePay  = require("../app").startTimeToBedtimePay ;
+const bedtimeToMidnightPay = require("../app").bedtimeToMidnightPay;
+const midnightToEndOfJobPay = require("../app").midnightToEndOfJobPay;
+
+
+
+describe('App Tests', ()=>{
+    describe('The BabySitter', () => {
+
+        it('should be paid $12 for one hour of work before bedtime and before midnight', () => {
+          var results  = app(setHour(5), setHour(6), setHour(10));
+      
+          assert.equal(results, 12);
+        });
+
+        it('should be paid $24 for two hours of work before bedtime', () => {
+            var results  = app (setHour(5), setHour(7), setHour(10));
+        
+            assert.equal(24, results);
+          });
+
+          it('should be paid $8 for one hour of work after bedtime', () => {
+            var results  = app (setHour(7), setHour(8), setHour(6));
+        
+            assert.equal(8, results);
+          });
+        
+          it('should be paid $20 for 1 hour before bedtime and 1 hour after bedtime', () => {
+            var results  = app(setHour(5), setHour(7), setHour(6));
+        
+            assert.equal(20, results);
+          });
+        
+          it('should be paid $28 for 1 hour before bedtime and 2 hours after bedtime', () => {
+            var results  = app(setHour(5), setHour(8), setHour(6));
+        
+            assert.equal(28, results);
+          });
+        
+          it('should be paid $16 for 1 hour after midnight', () => {
+            var results  = app(setHour(12), setHour(1), setHour(1));
+        
+            assert.equal(16, results);
+          });
+        
+          it('should be paid $28 for 1 hour awake before midnight and 1 hour after midnight', () => {
+            var results  = app(setHour(11), setHour(1), setHour(12));
+        
+            assert.equal(28, results);
+          });
+        
+          it('should be paid $24 for 1 hour in bed before midnight and 1 hour after midnight', () => {
+            var results  = app(setHour(11), setHour(1), setHour(11));
+        
+            assert.equal(24, results);
+          });
+        
+          it('should not be paid for not working any hours', () => {
+            var results  = app(setHour(7), setHour(7), setHour(8));
+        
+            assert.equal(0, results);
+        
+        });
     });
 });
+
+function setHour(hour) {
+    var date = new Date();
+    date.setHours(hour);
+    date.setMinutes(0);
+    date.setSeconds(0);
+    
+    return date;
+  }
