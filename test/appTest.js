ca6bbb9c2ea7989dca48d8ccff623f913f6381fe
DeeKozman <dankoscielak@gmail.com>
2018-04-03 18:57:18 -0400

    fixed tests although all are not passing.

--- test/appTest.js
+++ test/appTest.js
@@ -37,68 +37,53 @@ describe('App Tests', () => {
             expect(app.bind(null, new moment('2018-01-02T17:00:00'), new moment('2018-01-01T18:00:00'))).to.throw('Invalid shift length.');
         });
 
-        it('should be paid $24 for two hours of work before bedtime', () => {
-            var results = app(setMoment(5), setMoment(7), bedTimeHour);
-
+        it('should be paid $24 for two hours of work before bedtime at the rate of $12 an hour', () => {
+            var results = app(new moment('2018-01-01T17:00:00'), new moment('2018-01-01T19:00:00'));
             assert.equal(24, results);
         });
 
-        it('should be paid $8 for one hour of work after bedtime', () => {
-            var results = app(setMoment(7), setMoment(8), bedTimeHour);
+        it('should be paid $8 for one hour of work after bedtime at the rate of $8 an hour', () => {
+            var results = app(new moment('2018-01-01T21:00:00'), new moment('2018-01-01T22:00:00'));
 
             assert.equal(8, results);
         });
 
-        it('should be paid $20 for 1 hour before bedtime and 1 hour after bedtime', () => {
-            var results = app(setMoment(5), setMoment(7), bedTimeHour);
+        it('should be paid $20 for 1 hour before bedtime at the rate of $12 an hour and 1 hour after bedtime at the rate of $8 an hour', () => {
+            var results = app(new moment('2018-01-01T19:00:00'), new moment('2018-01-01T20:00:00'));
 
             assert.equal(20, results);
         });
 
-        it('should be paid $28 for 1 hour before bedtime and 2 hours after bedtime', () => {
-            var results = app(setMoment(5), setMoment(8), bedTimeHour);
+        it('should be paid $28 for 1 hour before bedtime at the rate of $12 an hour and 2 hours after bedtime at the rate of $8 an hour', () => {
+            var results = app(new moment('2018-01-01T19:00:00'), new moment('2018-01-01T21:00:00'));
 
             assert.equal(28, results);
         });
 
-        it('should be paid $16 for 1 hour after midnight', () => {
-            var results = app(setMoment(12), setMoment(1), bedTimeHour);
+        it('should be paid $16 for 1 hour after midnight at the rate of $16 an hour', () => {
+            var results = app(new moment('2018-01-02T00:00:00'), new moment('2018-01-02T01:00:00'));
 
             assert.equal(16, results);
         });
 
-        it('should be paid $28 for 1 hour awake before midnight and 1 hour after midnight', () => {
-            var results = app(setMoment(11), setMoment(1), bedTimeHour);
-
-            assert.equal(28, results);
-        });
-
-        it('should be paid $24 for 1 hour in bed before midnight and 1 hour after midnight', () => {
-            var results = app(setMoment(11), setMoment(1), bedTimeHour);
+        it('should be paid $24 for 1 hour after bedtime before midnight and 1 hour after midnight', () => {
+            var results = app(new moment('2018-01-01T23:00:00'), new moment('2018-01-02T00:00:00'));
 
             assert.equal(24, results);
         });
 
+        it('should be paid $64 for 4 hours after midnight at the rate of $16 an hour', () => {
+            var results = app(new moment('2018-01-02T00:00:00'), new moment('2018-01-02T04:00:00'));
+
+            assert.equal(64, results); 
+        });
+
         it('should not be paid for not working any hours', () => {
-            var results = app(setMoment(7), setMoment(7), bedTimeHour);
+            var results = app(new moment('2018-01-01T17:00:00'), new moment('2018-01-01T17:00:00'));
 
             assert.equal(0, results);
 
         });
     });
-    describe('The Time', () => {
-       
-    });
+    
 });
-
-//creates the times used in the results variable in the test cases.
-function setMoment(hour) {
-    //console.log(hour.toString());
-    var m = moment(new Date()).hour(hour);
-    console.log(m.hours().toString());
-    //var nextM = m.clone().add(1,'day').FromNow();
-    //onsole.log(nextM.toString());
-    //var m = moment().add(hour, 'h');
-
-    return m;
-};
